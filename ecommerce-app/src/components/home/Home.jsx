295f130017672a304e081835f91e3efda945ed8d
YMarianaAguilar <ymarianaaguilar@gmail.com>
2023-10-26 19:00:09 -0300

    Se agrego el navbar, un mensaje de bienvenida y tres cards

---
+++ ecommerce-app/src/components/home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./home.css";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+const Home = () => {
+    return (
+        <div>
+            <div className="jumbotron">
+                <h1 className="display-4">¡Bienvenidos a COSMETICA!</h1>
+                <p className="lead">Aquí podrás encontrar productos de belleza y cuidado personal adecuados para todo tipo de piel y preferencia, además de grandes decuentos.</p>
+                <hr className="my-4" />
+                <p>Si desea un asesoramiento personalizado podés comunicarte con nosotros, estaremos encantados de atenderte.</p>
+                <a className="btn btn-primary btn-lg" href="#" role="button">Contacto</a>
+            </div>
+        </div>
+    );
+};
+
+export default Home
