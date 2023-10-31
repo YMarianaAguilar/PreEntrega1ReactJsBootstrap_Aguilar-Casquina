7fa7911af4de3aa8178da0625f157cda6acf8980
YMarianaAguilar <ymarianaaguilar@gmail.com>
2023-10-31 17:24:07 -0300

    se agregó el componente CartWidget en el NavBar

---
+++ ecommerce-app/src/components/header/cart-widget/CartWidget.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./cartWidget.css";
+
+const CartWidget = () => {
+  return (
+    <div><i class="bi bi-cart3"></i></div>
+  )
+}
+
+export default CartWidget
--- ecommerce-app/src/components/home/Home.jsx
+++ ecommerce-app/src/components/home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <div>
             <div className="jumbotron">
                 <h1 className="display-4">¡Bienvenidos a COSMETICA!</h1>
-                <p className="lead">Aquí podrás encontrar productos de belleza y cuidado personal adecuados para todo tipo de piel y preferencia, además de grandes decuentos.</p>
+                <p className="lead">Aquí podrás encontrar productos de belleza y cuidado personal adecuados para todo tipo de piel y preferencia, además de grandes descuentos.</p>
                 <hr className="my-4" />
                 <p>Si desea un asesoramiento personalizado podés comunicarte con nosotros, estaremos encantados de atenderte.</p>
                 <a className="btn btn-primary btn-lg" href="#" role="button">Contacto</a>
